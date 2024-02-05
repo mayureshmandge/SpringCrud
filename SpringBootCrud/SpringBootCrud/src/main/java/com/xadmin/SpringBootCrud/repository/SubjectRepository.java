cf4420898bd41c82eb3c95f00c7bba3f5eec09dc
Mayuresh Mandge <68776807+mayureshphp@users.noreply.github.com>
2024-02-05 22:44:54 +0530

    Add files via upload

---
+++ SpringBootCrud/SpringBootCrud/src/main/java/com/xadmin/SpringBootCrud/repository/SubjectRepository.java
@@ -0,0 +1,8 @@
+package com.xadmin.SpringBootCrud.repository;
+import org.springframework.data.repository.CrudRepository;
+
+import com.xadmin.SpringBootCrud.Bean.Subject;
+
+public interface SubjectRepository extends CrudRepository<Subject,String>{
+
+}
